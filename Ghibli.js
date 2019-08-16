0d4bafb4a3431b36319f1bf4bebe1e27fe94a24f
Jian <jianxie6464@gmail.com>
2019-08-15 23:22:47 -0700

    new branch version 1

---
+++ Ghibli.js
@@ -0,0 +1,16 @@
+let request = new XMLHttpRequest()
+
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+
+request.onload = function(){
+    let data = JSON.parse(this.response)
+    if(request.status >= 200 && request.status < 400){
+        data.forEach(movie => {
+            console.log(movie.title)
+        })
+    }else{
+        console.log("error")
+    }
+}
+
+request.send()
