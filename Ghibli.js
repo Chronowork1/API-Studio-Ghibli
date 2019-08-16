9f2802133e0848a3dd59627eae11ac1c931c86f3
Jian <jianxie6464@gmail.com>
2019-08-16 02:29:33 -0700

    Finished Studio Ghibli api version 1

--- Ghibli.js
+++ Ghibli.js
@@ -1,3 +1,13 @@
+const app = document.getElementById("root")
+const logo = document.createElement("img")
+logo.src = "https://taniarascia.github.io/sandbox/ghibli/logo.png"
+
+const container = document.createElement("div")
+container.setAttribute("class", "container")
+
+app.appendChild(logo)
+app.appendChild(container)
+
 let request = new XMLHttpRequest()
 
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
@@ -7,10 +17,27 @@ request.onload = function(){
     if(request.status >= 200 && request.status < 400){
         data.forEach(movie => {
             console.log(movie.title)
+            const card = document.createElement("div")
+            card.setAttribute("class", "card")
+
+            const h1 = document.createElement("h1")
+            h1.textContent = movie.title
+
+            const p = document.createElement("p")
+            movie.description = movie.description.substring(0, 300)
+            p.textContent = `${movie.description}...`
+
+            container.appendChild(card)
+
+            card.appendChild(h1)
+            card.appendChild(p)
         })
     }else{
-        console.log("error")
+        const errorMessage = document.createElement("marquee")
+        errorMessage.textContent = "Gah, It's not working!"
+        applicationCache.appendChild(errorMessage)
     }
 }
 
 request.send()
+
